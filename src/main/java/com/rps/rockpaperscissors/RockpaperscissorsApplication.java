8fc250adc9fc3289050dd24f10ec94990e22e4c2
Zach Urso <zacharyjurso@gmail.com>
2021-05-12 22:20:49 -0400

    first commit

---
+++ src/main/java/com/rps/rockpaperscissors/RockpaperscissorsApplication.java
@@ -0,0 +1,13 @@
+package com.rps.rockpaperscissors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockpaperscissorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockpaperscissorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rps/rockpaperscissors/RockpaperscissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rps.rockpaperscissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockpaperscissorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
