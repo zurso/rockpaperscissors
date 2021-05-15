603d6541f8bb4281469689542ce52cf2bc646cc9
Zach Urso <zacharyjurso@gmail.com>
2021-05-15 12:48:44 -0400

    RPS buttons return values

--- react-rps/src/components/Game.js
+++ react-rps/src/components/Game.js
@@ -4,14 +4,14 @@ import scissors from '../assets/images/icon-scissors.svg';
 
 const Game = () => {
     const shoot = (e) => {
-        console.log(e.target.value); //not working
+        console.log(e.currentTarget.value);
     }
 
     return (
         <div>
-            <button value="rock"><img src={rock} className="button" alt="rock"  onClick={e => shoot(e, "value")}/></button>
-            <button value="paper"><img src={paper} className="button" alt="paper" onClick={e => shoot(e, "value")}/></button>
-            <button value="scissors"><img src={scissors} className="button" alt="scissors" onClick={e => shoot(e, "value")}/></button>
+            <button value="rock" onClick={(e) => shoot(e)}><img src={rock} alt="rock"/></button>
+            <button value="paper" onClick={(e) => shoot(e)}><img src={paper} alt="paper"/></button>
+            <button value="scissors" onClick={(e) => shoot(e)}><img src={scissors} alt="scissors"/></button>
         </div>
     )
 }
